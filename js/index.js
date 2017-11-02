aca6705bd8a792a0d924bd39c662170da29f5bc7
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-11-02 19:52:00 -0400

    removed extra js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,6 @@
 $(document).ready(function(){
 
 
-// $(window).on('scroll',function(e){
-//     parallaxScroll();
-// });
-
-// function parallaxScroll(){
-//     var scrolled = $(window).scrollTop();
-//     $('#menu-body').css('top',(0-(scrolled*.1))+'px');
-//     $('#menu-items').css('top',(0-(scrolled*.75))+'px');
-
-// }
-
-// $(window).on('scroll',function(){
-//     parallaxScroll();
-// });
-
-// function parallaxScroll(){
-//     var scrolled = $(window).scrollTop();
-//     $('#menu-body').css('top',(0-(scrolled*.1))+'px');
-//     $('#menu-items').css('top',(0-(scrolled*.75))+'px');
-
-// }
-
 $(window).on('scroll',function(){
     parallaxScroll();
 });
